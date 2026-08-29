af249a80536818c7b941e0fb88347d2e9967f962
Vidharshana06-web <vidharshanadevaraj06@gmail.com>
2026-08-29 22:33:21 +0530

    commits

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath('/admin/queues');
 
 createBullBoard({
-  queues: [new BullMQAdapter(emailQueue)],
+  queues: [new BullMQAdapter(emailQueue) as any],
   serverAdapter: serverAdapter,
 });
 
